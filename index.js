4321341a8eed9315eb90440e742a01174cda70bb
sayemaa <sayema990@gmail.com>
2022-05-25 16:49:02 +0600

    orders added to db

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 // middleware
 app.use(cors());
 app.use(express.json());
-g
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s1ebc.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -18,6 +18,7 @@ async function run() {
     try {
         await client.connect();
         const partsCollection = client.db('manufacturerDB').collection('parts');
+        const orderCollection = client.db('manufacturerDB').collection('orders');
 
         // Get parts
         app.get('/parts', async (req, res) => {
@@ -27,11 +28,19 @@ async function run() {
             res.send(parts);
         });
 
+        // Get each parts
         app.get('/parts/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const eachParts = await partsCollection.findOne(query);
             res.send(eachParts);
+        })
+
+        // Create orders
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = orderCollection.insertOne(order);
+            res.send(result);
         })
     }
     finally {
