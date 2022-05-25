cdae28239f9eafea33c057eb13cef7603c56dd31
sayemaa <sayema990@gmail.com>
2022-05-26 02:07:36 +0600

    get admin from users

--- index.js
+++ index.js
@@ -113,6 +113,14 @@ async function run() {
             }
         })
 
+        // Get Admin
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
+
     }
     finally {
 
