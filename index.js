3a34fe04e73e5ca5d42d5a83ec6da977b5562a66
sayemaa <sayema990@gmail.com>
2022-05-25 21:11:39 +0600

    users added to db

--- index.js
+++ index.js
@@ -52,6 +52,19 @@ async function run() {
             res.send(orders);
         })
 
+        // PUT user into db
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        })
+
     }
     finally {
 
